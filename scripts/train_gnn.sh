cea38f8db42180791aa91de043298d6ccb455fc5
amitay <amitay.sich@gmail.com>
2024-05-28 08:24:02 +0300

    refactor the code

--- scripts/train_gnn.sh
+++ scripts/train_gnn.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+cd "$SCRIPT_DIR"
+pip install -e .
 
 num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 echo "Number of GPUs found: $num_gpus"
 
 gpus=($(seq 0 $((num_gpus - 1))))
 scripts=(
-    "python train_gnn.py --pretrained_method 0 --fuse_name mp_recon --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 1 --fuse_name mp_recon --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 2 --fuse_name all_recon --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 2 --fuse_name mp_recon --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 2 --fuse_name mp --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 2 --fuse_name all --train_all_emd 0"
-    "python train_gnn.py --pretrained_method 0 --fuse_name mp_recon --train_all_emd 1"
-    "python train_gnn.py --pretrained_method 1 --fuse_name mp_recon --train_all_emd 1"
+    "python model/train_gnn.py --pretrained_method 0 --fuse_name mp_recon --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 1 --fuse_name mp_recon --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 2 --fuse_name all_recon --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 2 --fuse_name mp_recon --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 2 --fuse_name mp --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 2 --fuse_name all --train_all_emd 0"
+    "python model/train_gnn.py --pretrained_method 0 --fuse_name mp_recon --train_all_emd 1"
+    "python model/train_gnn.py --pretrained_method 1 --fuse_name mp_recon --train_all_emd 1"
 )
 
 for i in ${!scripts[@]}; do
