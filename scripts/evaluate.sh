1f8ebaf39ae133d953c27e7c7bd32b1d3e3ce53e
amitay <amitay.sich@gmail.com>
2024-05-28 13:34:06 +0300

    refactor the code

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-# Array of GPU indices
-gpus=(0 1 2 3 4 5 6 7)
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+cd "$SCRIPT_DIR"
+pip install -e .
 
 num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 echo "Number of GPUs found: $num_gpus"
 
 gpus=($(seq 0 $((num_gpus - 1))))
 scripts=(
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_1024_1_1-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_3-sample_0-train_all_emd_0_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_256_0_0-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_2-sample_0-train_all_emd_0_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_1024_1_1-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_0-sample_0-train_all_emd_1_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_256_1_1-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_2-sample_0-train_all_emd_0_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_1024_1_1-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_0-sample_0-train_all_emd_0_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_1024_1_1-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_1-sample_0-train_all_emd_0_4"
-    "python eval_model.py --model_name model_fake_fake_task_1-fuse_config_8192_0.0_512_0.001_1_1024_1_0-hidden_channels_256-layer_type_SAGEConv-learned_embedding_dim_256-lr_0.001-num_layers_3-pretrained_method_2-sample_0-train_all_emd_0_4"
+    "python model/eval_model.py --model_name gnn_no_pretrain_freeze"
+    "python model/eval_model.py --model_name gnn_pretrain_freeze"
+    "python model/eval_model.py --model_name gnn_fuse_all_recon_freeze"
+    "python model/eval_model.py --model_name gnn_fuse_mp_recon_freeze"
+    "python model/eval_model.py --model_name gnn_fuse_mp_freeze"
+    "python model/eval_model.py --model_name gnn_fuse_all_freeze"
+    "python model/eval_model.py --model_name gnn_no_pretrain_train"
+    "python model/eval_model.py --model_name gnn_pretrain_train"
 )
 
 for i in ${!scripts[@]}; do
