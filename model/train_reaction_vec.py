eed84661240ed99cf0f9d69469867276d3b66ecc
amitay <amitay.sich@gmail.com>
2024-05-29 19:05:57 +0300

    add reaction bs task

--- model/train_reaction_vec.py
+++ model/train_reaction_vec.py
@@ -97,7 +97,11 @@ def evaluate(model, dataset: EmdDataset):
         sim = sim.cpu().numpy()
         order = list(np.argsort(sim))[::-1]
         indexes.append(order.index(index))
-    print(np.mean(indexes))
+    top_1= np.mean([1 for i in indexes if i < 1])
+    top_5 = np.mean([1 for i in indexes if i < 5])
+    top_10 = np.mean([1 for i in indexes if i < 10])
+    top_50 = np.mean([1 for i in indexes if i < 50])
+    print(f"Top 1: {top_1}, Top 5: {top_5}, Top 10: {top_10}, Top 50: {top_50}, Mean Index: {np.mean(indexes)}")
 
     dataset.return_index = False
 
