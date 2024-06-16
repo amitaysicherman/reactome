2983fddd8199c5a35de039cd271c9c030119701e
amitay <amitay.sich@gmail.com>
2024-06-16 10:28:46 +0300

    train with protein only

--- scripts/model_pipline.py
+++ scripts/model_pipline.py
@@ -1,21 +1,23 @@
 import torch
 import os
+import subprocess
 
 counter = 0
 
 
-def get_defult_args():
-    return {'name': 'default',
-            'fuse_name': '',
-            'fuse_recon': 0,
-            'gnn_train_all_emd': 0,
-            'gnn_hidden_channels': 256,
-            'fuse_all_to_one': '',
-            'gnn_pretrained_method': 1,
-            'data_aug': 'all',
-            'gnn_last_or_concat': 0,
-            'gnn_reaction_or_mean': 0,
-            }
+def get_default_args():
+    return {
+        'name': 'default',
+        'fuse_name': '',
+        'fuse_recon': 0,
+        'gnn_train_all_emd': 0,
+        'gnn_hidden_channels': 256,
+        'fuse_all_to_one': '',
+        'gnn_pretrained_method': 1,
+        'data_aug': 'all',
+        'gnn_last_or_concat': 0,
+        'gnn_reaction_or_mean': 0,
+    }
 
 
 def fill_node_emd_args(args, node_emd):
@@ -42,7 +44,6 @@ def fill_node_emd_args(args, node_emd):
         elif node_emd == "all-to-all":
             args['fuse_all_to_one'] = 'all'
             args['fuse_name'] = "all"
-    args['fuse_name'] = "" #TODO: remove this line for running one bt one ?
 
 
 def fill_size_args(args, model_size):
@@ -75,32 +76,61 @@ def args_to_str(args):
     for key, value in args.items():
         if value != "":
             args_str += f"--{key} {value} "
-    return args_str
+    return args_str.strip()
 
 
 def get_args(node_emd, model_size, graph_emb, aug_data):
-    args = get_defult_args()
+    args = get_default_args()
     fill_node_emd_args(args, node_emd)
     fill_size_args(args, model_size)
     fill_graph_emb_args(args, graph_emb)
     fill_aug_data_args(args, aug_data)
     name = f"{node_emd}_{model_size}_{graph_emb}_{aug_data}"
     args['name'] = name
-    args = args_to_str(args)
-    return args
+    return args_to_str(args)
 
 
 num_gpus = torch.cuda.device_count()
+max_concurrent_runs = 64
 
+
+def run_commands(commands):
+    processes = []
+    for cmd in commands:
+        processes.append(subprocess.Popen(cmd, shell=True))
+        if len(processes) >= max_concurrent_runs:
+            for p in processes:
+                p.wait()
+            processes = []
+    for p in processes:
+        p.wait()
+
+
+# Collect commands for the first loop
+commands = []
+for i, name in enumerate(["no", "fuse", "recon", "all-to-prot", "all-to-mol", "all-to-all"]):
+    args = get_default_args()
+    fill_node_emd_args(args, name)
+    args['name'] = name
+    gpu_index = i % num_gpus
+    script = f"python model/contrastive_learning.py --name {name} &&"
+    cmd = f'CUDA_VISIBLE_DEVICES="{gpu_index}" bash -c "{script}" &'
+    commands.append(cmd)
+run_commands(commands)
+
+commands = []
 for node_emd in ["no", "pre", "fuse", "recon", "all-to-prot", "all-to-mol", "all-to-all"]:
     for model_size in ["s", "m", "l"]:
         for graph_emb in ["reaction", "mean", "concat", "both"]:
-            for aug_data in ["all", "prot", "mol", 'location']:
+            for aug_data in ["all", "prot", "mol", "location"]:
                 args = get_args(node_emd, model_size, graph_emb, aug_data)
-                script = f"python model/contrastive_learning.py {args} && python model/train_gnn.py {args} && python model/eval_model.py {args}"
+                script = f"python model/train_gnn.py {args} && python model/eval_model.py {args}"
                 counter += 1
                 gpu_index = counter % num_gpus
                 cmd = f'CUDA_VISIBLE_DEVICES="{gpu_index}" bash -c "{script}" &'
-                print(cmd)
-                os.system(cmd)
+                commands.append(cmd)
+
+# Run the second set of commands
+run_commands(commands)
+
 print(counter)
