987fd7376c6cc9a52f68a014714bb7a2e8a7be82
amitay <amitay.sich@gmail.com>
2024-06-19 13:56:15 +0300

    train with protein only

--- scripts/all_fuse.sh
+++ scripts/all_fuse.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #SBATCH --time=1-0
 #SBATCH --array=1-6
-#SBATCH --requeue
 #SBATCH --gres=gpu:A40:1
 #SBATCH --mem=64G
 
---
+++ scripts/all_gnn.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#SBATCH --time=1-0
+#SBATCH --array=1-6
+#SBATCH --gres=gpu:A40:1
+#SBATCH --mem=64G
+#SBATCH -c 24
+
+python3 scripts/model_pipline.py --index $(($SLURM_ARRAY_TASK_ID - 1))
