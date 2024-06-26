5d212c7240bebc93d3465ddafecbca629a51c76f
amitay <amitay.sich@gmail.com>
2024-06-26 20:52:36 +0300

    constractive learning - remove DNA, text only act and not mod

--- dataset/seq_bp.py
+++ dataset/seq_bp.py
@@ -259,9 +259,9 @@ if __name__ == "__main__":
         epoch_score = run_epoch(**epoch_args, X=X_val, y=y_val, part="valid")
         run_epoch(**epoch_args, X=X_test, y=y_test, part="test")
 
-        if epoch_score > best_score:
-            best_score = epoch_score
-            torch.save(model.state_dict(), f"{save_dir}/{epoch}.pt")
-            if best_prev_index != -1:
-                os.remove(f"{save_dir}/{best_prev_index}.pt")
-            best_prev_index = epoch
+        # if epoch_score > best_score:
+        #     best_score = epoch_score
+        #     torch.save(model.state_dict(), f"{save_dir}/{epoch}.pt")
+        #     if best_prev_index != -1:
+        #         os.remove(f"{save_dir}/{best_prev_index}.pt")
+        #     best_prev_index = epoch
