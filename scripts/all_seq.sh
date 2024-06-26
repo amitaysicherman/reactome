669eb0b96092360a850edca222f39a2661ddbabc
amitay <amitay.sich@gmail.com>
2024-06-26 09:17:06 +0300

    constractive learning - remove DNA, text only act and not mod

--- scripts/all_seq.sh
+++ scripts/all_seq.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 #SBATCH --time=0-3
-#SBATCH --array=1-54
+#SBATCH --array=1-8
 #SBATCH --gres=gpu:A40:1
 #SBATCH --mem=64G
 #SBATCH -c 4
 #SBATCH --requeue
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/all_seq.txt)
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
+python3 dataset/seq_dataset.py $args
 python3 dataset/seq_dataset.py $args
