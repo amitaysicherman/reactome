5289382e0f2715538ac867b97ae4c5f2a8de5c82
amitay <amitay.sich@gmail.com>
2024-06-30 14:34:42 +0300

    constractive learning - remove DNA, text only act and not mod

--- common/args_manager.py
+++ common/args_manager.py
@@ -17,7 +17,7 @@ def get_args():
     parser.add_argument("--fuse_hidden_dim", type=int, default=512)
     parser.add_argument("--fuse_recon", type=int, default=0)
     parser.add_argument("--fuse_all_to_one", type=str, default="")
-    parser.add_argument("--fuse_self_move", type=str, default=0)
+    parser.add_argument("--fuse_self_move", type=str, default=1)
 
     parser.add_argument("--fuse_epochs", type=int, default=50)
     parser.add_argument("--fuse_name", type=str, default="")
