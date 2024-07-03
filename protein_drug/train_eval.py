f0582b022996593bbba02e1a43b9f0653e715b9b
amitay <amitay.sich@gmail.com>
2024-07-03 13:42:27 +0300

    commit before start to play with more complex fuse

--- protein_drug/train_eval.py
+++ protein_drug/train_eval.py
@@ -213,12 +213,10 @@ def run_epoch(model, loader, optimizer, criterion, part):
 
     auc = defaultdict(float)
     key=0 if part=="train" else 1
-
     real, pred = np.array(reals[key]), np.array(preds[key])
     auc[key] = roc_auc_score(real, pred)
-    auc=roc_auc_score(real, pred)
     precision, recall, thresholds = precision_recall_curve(real, pred)
-    print(f"{part} AUC: {auc*100:.1f} accuracy: {accuracy_score(real, pred > 0.5)*100:.1f} precision: {precision_score(real, pred > 0.5)*100:.1f} recall: {recall_score(real, pred > 0.5)*100:.1f} AUPR: {area_under_curve(recall, precision)*100:.1f")
+    print(f"{part} AUC: {auc[key]*100:.1f} accuracy: {accuracy_score(real, pred > 0.5)*100:.1f} precision: {precision_score(real, pred > 0.5)*100:.1f} recall: {recall_score(real, pred > 0.5)*100:.1f} AUPR: {area_under_curve(recall, precision)*100:.1f}")
 
     return auc
 
