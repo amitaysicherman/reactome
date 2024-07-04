5dba6c43b447a65df2ba1854229999d48a2bf2c3
amitay <amitay.sich@gmail.com>
2024-07-04 11:41:08 +0300

    commit before start to play with more complex fuse

--- protein_drug/prep.py
+++ protein_drug/prep.py
@@ -1,4 +1,4 @@
-from preprocessing.seq_to_vec import Seq2Vec
+from preprocessing.seq_to_vec import Seq2Vec, protein_name_to_cp
 from common.path_manager import data_path
 import os
 from tqdm import tqdm
@@ -7,15 +7,14 @@ import numpy as np
 from common.args_manager import get_args
 
 if __name__ == "__main__":
-
     args = get_args()
-    dataset = args.db_dataset
-
     self_token = args.self_token
     protein_emd = args.protein_emd
+    assert protein_emd in protein_name_to_cp, f"Unknown protein embedding: {protein_emd}"
     data_type = args.prep_reactome_dtype
-    name = args.prep_reactome_name
     seq2vec = Seq2Vec(self_token, protein_emd)
+
+    dataset = args.db_dataset
 
     base_dir = os.path.join(data_path, "protein_drug")
     input_file = os.path.join(base_dir, f"{dataset}.txt")
@@ -42,12 +41,12 @@ if __name__ == "__main__":
         molecules_vec.append(seq2vec.to_vec(smiles, MOLECULE))
         proteins_vec.append(seq2vec.to_vec(fasta, PROTEIN))
 
-    with open(os.path.join(base_dir, f"{dataset}_{name}_molecules.txt"), "w") as f:
+    with open(os.path.join(base_dir, f"{dataset}_molecules.txt"), "w") as f:
         f.write("\n".join(molecules))
-    with open(os.path.join(base_dir, f"{dataset}_{name}_proteins.txt"), "w") as f:
+    with open(os.path.join(base_dir, f"{dataset}_{protein_emd}_proteins.txt"), "w") as f:
         f.write("\n".join(proteins))
-    with open(os.path.join(base_dir, f"{dataset}_{name}_labels.txt"), "w") as f:
+    with open(os.path.join(base_dir, f"{dataset}_labels.txt"), "w") as f:
         f.write("\n".join(labels))
 
-    np.save(os.path.join(base_dir, f"{dataset}_{name}_molecules.npy"), np.array(molecules_vec))
-    np.save(os.path.join(base_dir, f"{dataset}_{name}_proteins.npy"), np.array(proteins_vec))
+    np.save(os.path.join(base_dir, f"{dataset}_molecules.npy"), np.array(molecules_vec))
+    np.save(os.path.join(base_dir, f"{dataset}_{protein_emd}_proteins.npy"), np.array(proteins_vec))
