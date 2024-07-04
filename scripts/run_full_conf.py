de6c15614a11fb49f672321c1be6f415fcdb59a9
amitay <amitay.sich@gmail.com>
2024-07-04 14:21:02 +0300

    commit before start to play with more complex fuse

---
+++ scripts/run_full_conf.py
@@ -0,0 +1,35 @@
+from common.args_manager import get_args
+import os
+
+
+def args_to_string(args):
+    res = ""
+    for arg in vars(args):
+        res += f"--{arg} {getattr(args, arg)} "
+
+
+args = get_args()
+
+# run fusing
+cmd = f"python3 model/contrastive_learning.py {args_to_string(args)}"
+print("Running command: ", cmd)
+os.system(cmd)
+
+# run drug protein
+args.dp_print = 1
+args.dp_fuse_base = f"{args.dp_fuse_base}_best"
+configs = [{"dp_m_fuse": 1, "dp_p_fuse": 1, "dp_m_model": 1, "dp_p_model": 1},
+           {"dp_m_fuse": 1, "dp_p_fuse": 1, "dp_m_model": 0, "dp_p_model": 0},
+           {"dp_m_fuse": 0, "dp_p_fuse": 0, "dp_m_model": 1, "dp_p_model": 1},
+           {"dp_m_fuse": 1, "dp_p_fuse": 1, "dp_m_model": 0, "dp_p_model": 1},
+           {"dp_m_fuse": 1, "dp_p_fuse": 1, "dp_m_model": 1, "dp_p_model": 0},
+           {"dp_m_fuse": 0, "dp_p_fuse": 1, "dp_m_model": 1, "dp_p_model": 1},
+           {"dp_m_fuse": 1, "dp_p_fuse": 0, "dp_m_model": 1, "dp_p_model": 1}]
+for config in configs:
+    for random_seed in range(42, 52):
+        args.random_seed = random_seed
+        for key, value in config.items():
+            setattr(args, key, value)
+        cmd = f"python protein_drug/train_eval.py {args_to_string(args)}"
+        print("Running command: ", cmd)
+        os.system(cmd)
---
+++ scripts/run_full_conf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --time=3-00
+#SBATCH --array=1-15
+#SBATCH --gres=gpu:A4000:1
+#SBATCH --mem=64G
+#SBATCH --requeue
+
+args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/run_full_conf.txt)
+python3 scripts/run_full_conf.py $args
+
+
