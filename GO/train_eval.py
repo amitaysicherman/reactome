5d8516a4d44a170726bd3b42d39466b59b793059
amitay.s <amitay.sich@gmail.com>
2024-07-09 14:09:16 +0300

    add GO task

--- GO/train_eval.py
+++ GO/train_eval.py
@@ -177,8 +177,10 @@ def main(args, fuse_model=None):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     #calcualte pos_weight based on the data
-    pos_weight = labels.sum(axis=0) / labels.shape[0]
+    pos_weight = train_labels.sum(axis=0) / train_labels.shape[0]
+    pos_weight = (1 - pos_weight) / pos_weight
     pos_weight = torch.tensor(pos_weight, device=device, dtype=torch.float32)
+    print(pos_weight)
     loss_func = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
     best_val_score = 0
