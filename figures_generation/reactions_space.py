c44da7463eb6e6180fb3dccd3675c6f8b1bbb354
amitay <amitay.sich@gmail.com>
2024-07-16 23:51:06 +0300

    commit before start to play with more complex fuse ed

--- figures_generation/reactions_space.py
+++ figures_generation/reactions_space.py
@@ -50,7 +50,7 @@ def plot_reaction_space(counter, fuse_model, prot_emd_type, mol_emd_type, pretra
     cosine_dist = cosine_distances(vecs)
 
     # Initialize and fit t-SNE
-    tsne = TSNE(metric='precomputed', init="random", n_components=2, perplexity=min(len(vecs)//2, 30), n_iter=500,
+    tsne = TSNE(metric='precomputed', init="random", n_components=2, perplexity=3, n_iter=500,
                 verbose=0)
     X_embedded = tsne.fit_transform(cosine_dist)
 
