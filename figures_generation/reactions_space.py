d40fab631ef69f94d5daf27babc901a8f76abfd4
amitay <amitay.sich@gmail.com>
2024-07-18 01:57:47 +0300

    commit before start to play with more complex fuse ed

--- figures_generation/reactions_space.py
+++ figures_generation/reactions_space.py
@@ -14,7 +14,7 @@ ENTITIES = [PROTEIN, MOLECULE]
 
 COLORS = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999']
 
-reactions_ids = [1034, 222, 4355]
+reactions_ids = [100, 200, 400]
 
 
 def plot_reaction_space(counter, fuse_model, prot_emd_type, mol_emd_type, pretrained_method=2):
