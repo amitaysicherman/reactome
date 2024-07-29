4a5b541b29859d6855ad634ea6c77fd57fb23b97
amitay.s <amitay.sich@gmail.com>
2024-07-29 21:50:47 +0300

    add GO task

--- torchdrug_tasks/hyperparameter_search.py
+++ torchdrug_tasks/hyperparameter_search.py
@@ -55,7 +55,7 @@ def main(args):
         f.write(",".join(all_cols) + "\n")
 
 
-    for option in tqdm(all_options[:1]):
+    for option in tqdm(all_options):
         val_score, test_score = train_model_with_config(option, **args)
         values = [val_score, test_score] + [option.get(col, None) for col in config_cols]
         with open(filename, "a") as f:
