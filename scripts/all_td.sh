2eb21614008d78493cfeba831c93e639914a571e
amitay.s <amitay.sich@gmail.com>
2024-07-29 18:33:45 +0300

    add GO task

--- scripts/all_td.sh
+++ scripts/all_td.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 #SBATCH --time=3-00
 #SBATCH --array=1-19
-#SBATCH --gres=gpu:A4000:1
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --requeue
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/all_td.txt)
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
+python torchdrug_tasks/hyperparameter_search.py $args
