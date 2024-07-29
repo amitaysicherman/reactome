f85fbeda0f36ba04a43b5224bdf057ef6a88d2b5
amitay.s <amitay.sich@gmail.com>
2024-07-29 20:44:29 +0300

    add GO task

---
+++ scripts/all_td.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def submit_slurm_job(command, i):
+    """Submits a Slurm job using sbatch --wrap"""
+    slurm_command = f"sbatch job-name td-{i} --time=1-00 --mem=64G --requeue --cpus-per-task=8 --wrap 'python torchdrug_tasks/hyperparameter_search.py {command}'"
+    subprocess.run(slurm_command, shell=True, check=True)
+
+
+# Read commands from the text file
+with open("scripts/all_td.txt", "r") as f:
+    commands = f.readlines()
+
+# Submit Slurm jobs for each command
+for i, command in enumerate(commands):
+    command = command.strip()  # Remove leading/trailing whitespace
+    submit_slurm_job(command, i)
