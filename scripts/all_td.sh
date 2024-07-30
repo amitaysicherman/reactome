c29fe5653c40443cf2bc737bcda3d57bae733444
amitay.s <amitay.sich@gmail.com>
2024-07-30 08:15:54 +0300

    add GO task

--- scripts/all_td.sh
+++ scripts/all_td.sh
@@ -7,13 +7,22 @@
 #SBATCH --requeue
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/all_td.txt)
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
-python torchdrug_tasks/trainer.py $args
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
+
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
+
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
+
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
+
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
+python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
