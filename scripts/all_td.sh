c88a156dc4893a7bd688d9530a381d559338c93e
amitay.s <amitay.sich@gmail.com>
2024-07-31 14:08:19 +0300

    remove bn. remove transformer

--- scripts/all_td.sh
+++ scripts/all_td.sh
@@ -1,32 +1,12 @@
 #!/bin/sh
 #!/bin/sh
 #SBATCH --time=3-00
-#SBATCH --array=1-18
+#SBATCH --array=1-106
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:A4000:1
 #SBATCH --requeue
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/all_td.txt)
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
-
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
-
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
-
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
-
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
-python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
-
 python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
 python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 0
 python torchdrug_tasks/trainer.py $args --cafa_use_fuse 0 --cafa_use_model 1
