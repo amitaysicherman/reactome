160c5cd66dea615ce58c87e36d4fbd2e357e5f80
amitay.s <amitay.sich@gmail.com>
2024-08-01 20:09:30 +0300

    remove bn. remove transformer

--- torchdrug_tasks/scores_analyzer.py
+++ torchdrug_tasks/scores_analyzer.py
@@ -168,9 +168,10 @@ def add_ablation_col(data):
 
 # Load data
 data = pd.read_csv("data/scores/torchdrug.csv", on_bad_lines='warn')
-data=data[data['task_name'] != 'BACE']
 data = data.dropna()
 data = add_ablation_col(data)
+data = data[data['task_name'] != 'BACE']
+
 data = df_to_selected_matic(data)
 
 # Group by and apply aggregation
