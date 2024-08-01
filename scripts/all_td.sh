42603dc5eefd14238d9d8242820e816730f9f0b4
amitay.s <amitay.sich@gmail.com>
2024-08-01 13:18:11 +0300

    remove bn. remove transformer

--- scripts/all_td.sh
+++ scripts/all_td.sh
@@ -4,6 +4,7 @@
 #SBATCH --array=1-256
 #SBATCH --mem=16G
 #SBATCH --requeue
+#SBATCH --gres=gpu:A4000:1
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/all_td.txt)
 python torchdrug_tasks/trainer.py $args --cafa_use_fuse 1 --cafa_use_model 1
