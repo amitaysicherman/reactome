935adc2ea86f0f2ea7d15a38f41e06c1c21a1a3c
amitay.s <amitay.sich@gmail.com>
2024-08-01 12:46:48 +0300

    remove bn. remove transformer

--- scripts/contrastive_learning.sh
+++ scripts/contrastive_learning.sh
@@ -3,6 +3,7 @@
 #SBATCH --time=3-00
 #SBATCH --array=1-60
 #SBATCH --mem=32G
+#SBATCH --gres=gpu:A4000:1
 #SBATCH --requeue
 
 args=$(sed -n "$SLURM_ARRAY_TASK_ID"p scripts/contrastive_learning.txt)
